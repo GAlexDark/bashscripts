8008b15cc6a9a28ef4e4faf87b2c02c558a529db
Oleksii Gaienko <galexsoftware@gmail.com>
2023-04-25 13:22:33 +0300

    Fix other

--- cleaner/cleaner.sh
+++ cleaner/cleaner.sh
@@ -4,30 +4,30 @@ schedulerLogsDir="/logs/scheduler"
 dagProcessorManagerLogsDir="/logs/dag_processor_manager"
 #dagProcessorManagerLogsDir="/var/log/mysql"
 
-fileLog="/home/galex/projects/bashscripts/cleaner/info.log"
+fileLog="/var/log/cleaner_info.log"
 isDebug=0
 olderThan="+31"
 
-echo "$(date +%Y-%m-%d-%H-%M-%S) Start cleaning" >> $fileLog
+echo "$(date +%Y-%m-%d-%H-%M-%S) Start cleaning" >> "$fileLog"
 
 if [ -n "$1" ]
 then
     if [ "$1" = "--Debug" ]
     then
-        echo "Debug mode enabled." >> $fileLog
+        echo "Debug mode enabled." >> "$fileLog"
         isDebug=1
     else
-        echo "Unknown argument." >> $fileLog
+        echo "Unknown argument." >> "$fileLog"
     fi
 fi
 
 # ref: https://askubuntu.com/a/30157/8698
 if ! [ "$( id -u )" = 0 ]
 then
-  echo -e "This script must run with the Root privileges!\nThe script terminated.\n$(date +%Y-%m-%d-%H-%M-%S) End cleaning\n"  >> $fileLog
+  echo -e "This script must run with the Root privileges!\nThe script terminated.\n$(date +%Y-%m-%d-%H-%M-%S) End cleaning\n"  >> "$fileLog"
   #exit 1
 else
-    echo "The script runs with the Root privileges." >> $fileLog
+    echo "The script runs with the Root privileges." >> "$fileLog"
 fi
 
 #----------------------------------------------------------------------------
@@ -47,8 +47,8 @@ removeByNamePattern () {
 
   if [ "$isDebug" -eq 1 ]
   then
-    echo "Current year: $currentYear" >> $fileLog
-    echo "Current month: $currentMonth" >> $fileLog
+    echo "Current year: $currentYear" >> "$fileLog"
+    echo "Current month: $currentMonth" >> "$fileLog"
   fi
 
   if [ "$currentMonth" -eq "$january" ]
@@ -61,35 +61,35 @@ removeByNamePattern () {
   fi
   if [ "$isDebug" -eq 1 ]
   then
-    echo "Removed Year: $removedYear" >> $fileLog
-    echo "Removed Month: $removedMonth" >> $fileLog
+    echo "Removed Year: $removedYear" >> "$fileLog"
+    echo "Removed Month: $removedMonth" >> "$fileLog"
   fi
   #create pattern
-  pattern=$(printf "%d-%.2d-*" "$removedYear" "$removedMonth" >> $fileLog 2>&1)
+  pattern=$(printf "%d-%.2d-*" "$removedYear" "$removedMonth" >> "$fileLog" 2>&1)
   if [ -z "$pattern" ]
   then
-    echo -e "Wrong pattern. The script terminated.\n$(date +%Y-%m-%d-%H-%M-%S) End cleaning\n"  >> $fileLog
+    echo -e "Wrong pattern. The script terminated.\n$(date +%Y-%m-%d-%H-%M-%S) End cleaning\n"  >> "$fileLog"
     exit 1
   fi
   if [ "$isDebug" -eq 1 ]
   then
-    echo "The pattern for deleted directories: $pattern" >> $fileLog
+    echo "The pattern for deleted directories: $pattern" >> "$fileLog"
   fi
   #remove files and directories from pattern
-  find "$schedulerLogsDir" -maxdepth 1 -type d -iname "$pattern" -exec rm -rf {} \; >> $fileLog 2>&1
+  find "$schedulerLogsDir" -maxdepth 1 -type d -iname "$pattern" -exec rm -rf {} \; >> "$fileLog" 2>&1
 }
 #----------------------------------------------------------------------------
 # {
 #  echo "Remove scheduler logs in their folders older than $olderThan days"
 #  find "$schedulerLogsDir" -maxdepth 1 -type d -mtime "$olderThan" -exec rm -rf {} \;
 # } >> $fileLog 2>&1
-echo "Remove scheduler logs in their folders for pattern." >> $fileLog 2>&1
+echo "Remove scheduler logs in their folders for pattern." >> "$fileLog"
 removeByNamePattern
 #----------------------------------------------------------------------------
 {
   echo "Remove DAG Processor manager log files"
   find "$dagProcessorManagerLogsDir" -type f -iname '*.log.*' -mtime "$olderThan" -exec rm -rf {} \;
-} >> $fileLog 2>&1
+} >> "$fileLog" 2>&1
 #----------------------------------------------------------------------------
-echo -e "$(date +%Y-%m-%d-%H-%M-%S) End cleaning\n" >> $fileLog
+echo -e "$(date +%Y-%m-%d-%H-%M-%S) End cleaning\n" >> "$fileLog"
 exit 0
